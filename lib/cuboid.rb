14abf95ff6dceb0ca9a364715a6522507cc532ee
David James Carter <fresco.raja@gmail.com>
2015-09-08 01:12:16 -0700

    finalized rotation about x axis, got tests to pass"

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,17 +1,97 @@
-
 class Cuboid
-  
-  #BEGIN public methods that should be your starting point
+  #x: height, y: width, z: length
+  @@DFLT_BNDS = { x: 0, y: 0, z: 0 }
+  attr_reader :x, :y, :z, :mins, :maxes,
+    :height, :width, :len
+
+  def initialize(x, y, z, h = 2, w = 2, l = 2)
+    @x, @y, @z = x, y, z
+    @len, @width, @height = Float(l), Float(w), Float(h)
+    @maxes, @mins = {}, {}
+    set_bounds
+  end
+
+  def center
+    { x: @x, y: @y, z: @z }
+  end
+
+  def diag
+    Math.sqrt((@maxes[:y] - @y)**2 + (@maxes[:z] - @z)**2)
+  end
+
+  def intersects?(other)
+    (other.mins[:x] < @maxes[:x] && other.maxes[:x] > @mins[:x]) &&
+    (other.mins[:y] < @maxes[:y] && other.maxes[:y] > @mins[:y]) &&
+    (other.mins[:z] < @maxes[:z] && other.maxes[:z] > @mins[:z])
+  end
+
+  def move_to(x, y, z, bounds = @@DFLT_BNDS)
+    container = bounds
+    test_cube = Cuboid.new(x, y, z, @height, @width, @len)
+    if test_cube.will_fit?(container)
+      move_to!(x, y, z)
+      true
+    else
+      false
+    end
+  end
 
   def move_to!(x, y, z)
-  end
-  
-  def vertices
-  end
-  
-  #returns true if the two cuboids intersect each other.  False otherwise.
-  def intersects?(other)
+    @x, @y, @z = x, y, z
+    set_bounds
+    self.center
   end
 
-  #END public methods that should be your starting point  
+  def rotatex(bounds = @@DFLT_BNDS)
+    container = bounds
+    old_center = self.center
+    new_center = {}
+    if (container[:x] > @mins[:x])
+      new_center[:x] = container[:x] + @height / 2
+    end
+    [:y, :z].each do |axis|
+      new_center[axis] =
+        [
+          center[axis],
+          container[axis] + diag
+        ].max
+    end
+    new_center = old_center.merge(new_center)
+    if new_center == old_center
+      rotatex!
+      true
+    else
+      new_center.values
+    end
+  end
+
+  def rotatex!
+    @width, @len = @len, @width
+    set_bounds
+  end
+
+  def vertices
+    vertices = []
+    [@maxes[:x], @mins[:x]].each do |x|
+      [@maxes[:y], @mins[:y]].each do |y|
+        [@maxes[:z], @mins[:z]].each do |z|
+          vertices.push([x, y, z])
+        end
+      end
+    end
+
+    vertices
+  end
+
+  def will_fit?(bounds = @@DFLT_BNDS)
+    container = bounds
+    center.keys.none? { |axis| @mins[axis] < container[axis]}
+  end
+
+  private
+  def set_bounds
+    @maxes[:x], @mins[:x] = [@x + (@height / 2), @x - (@height / 2)]
+    @maxes[:y], @mins[:y] = [@y + (@width / 2), @y - (@width / 2)]
+    @maxes[:z], @mins[:z] = [@z + (@len / 2), @z - (@len / 2)]
+  end
 end
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -1,17 +1,222 @@
 require 'cuboid'
 
-#This test is incomplete and, in fact, won't even run without errors.  
+#This test is incomplete and, in fact, won't even run without errors.
 #  Do whatever you need to do to make it work and please add your own test cases for as many
 #  methods as you feel need coverage
 describe Cuboid do
- 
-  describe "move_to" do
-    it "changes the origin in the simple happy case" do
-      expect(subject.move_to!(1,2,3)).to be true
+  describe "#initialize" do
+    it "creates a cube with given center" do
+      cube = Cuboid.new(4,4,4)
+      expect(cube.center).to eq({x: 4, y: 4, z: 4})
     end
-  end    
-  
-  describe "intersects?" do
+
+    it "sets height, width, and length attributes" do
+      cube = Cuboid.new(3,10,12,6,12,8)
+      expect(cube.height).to eq(6)
+      expect(cube.width).to eq(12)
+      expect(cube.len).to eq(8)
+    end
+
+    it "computes max and min values or boundaries of cuboid" do
+      cube = Cuboid.new(3,3,3,6,6,6)
+      expect(cube.maxes).to eq({ x: 6, y: 6, z: 6 })
+      expect(cube.mins).to eq({ x: 0, y: 0, z: 0 })
+    end
   end
 
+  describe "#vertices" do
+    it "returns vertices (when no size provided l, w, h = 2)" do
+      cube = Cuboid.new(1,1,1)
+      expect(cube.vertices.sort).to eq([
+        [0,0,0],
+        [0,2,0],
+        [0,2,2],
+        [2,0,0],
+        [2,0,2],
+        [2,2,0],
+        [0,0,2],
+        [2,2,2]
+      ].sort)
+    end
+
+    it "computes vertices (when size provided)" do
+      cube = Cuboid.new(3,3,3,6,4,5)
+      expect(cube.vertices.sort).to eq([
+        [0,5,5.5],
+        [0,5,0.5],
+        [0,1,5.5],
+        [0,1,0.5],
+        [6,5,5.5],
+        [6,5,0.5],
+        [6,1,5.5],
+        [6,1,0.5]
+      ].sort)
+    end
+  end
+
+  describe "#will_fit?" do
+    it "returns true if cuboid is inside origin ([0,0,0] by default)" do
+      cube = Cuboid.new(3,4,5,3,3,4)
+      expect(cube.will_fit?).to be(true)
+    end
+
+    it "returns true if cuboid inside arbritrary specified origin" do
+      cube = Cuboid.new(0,0,0,4,4,4)
+      expect(cube.will_fit?({ x: -2, y: -2, z: -2 })).to be(true)
+    end
+
+    it "return false if cuboid is overlapping origin on x axis" do
+      cube = Cuboid.new(1,4,5,3,3,4)
+      expect(cube.mins[:x]).to be < 0
+      expect(cube.will_fit?).to be(false)
+    end
+
+    it "return false if cuboid is overlapping origin on y axis" do
+      cube = Cuboid.new(3,4,5,3,12,4)
+      expect(cube.mins[:y]).to be < 0
+      expect(cube.will_fit?).to be(false)
+    end
+
+    it "return false if cuboid is overlapping origin on z axis" do
+      cube = Cuboid.new(1,4,5,3,3,20)
+      expect(cube.mins[:z]).to be < 0
+      expect(cube.will_fit?).to be(false)
+    end
+  end
+
+  describe "#move_to!" do
+    it "always changes the origin" do
+      new_center = [-100,-100,-100]
+      cube = Cuboid.new(0,0,0,4,5,6)
+      cube.move_to!(*new_center)
+
+      expect(cube.center.values).to eq(new_center)
+    end
+  end
+
+  describe "#move_to" do
+    subject { Cuboid.new(5,5,5,10,10,10)}
+    it "returns true when move does not cause collision with boundary" do
+      expect(subject.move_to(50,50,50)).to be(true)
+    end
+
+    it "changes the center to specified value" do
+      subject.move_to(50,50,50)
+      expect(subject.center.values).to eq([50,50,50])
+    end
+
+    it "returns false if destination will cause overlap with boundary" do
+      expect(subject.move_to(-15,-15,-15)).to be(false)
+    end
+
+    it "does not move cuboid if overlap will occur" do
+      subject.move_to(-12,0,2)
+      expect(subject.center.values).to eq([5,5,5])
+    end
+
+    it "detects overlap with arbritray specified boundary" do
+      bounds = { x: 3, y: 1, z: 0 }
+      expect(subject.move_to(3,1,0, bounds)).to be(false)
+    end
+
+    it "allows move to center inside arbritrary specified boundary" do
+      bounds = { x: 1, y: 5, z: 10 }
+      expect(subject.move_to(6, 10, 15)).to be(true)
+      expect(subject.center.values).to eq([6,10,15])
+    end
+  end
+
+  describe "intersects?" do
+    it "detects intersection between two cuboids" do
+      cube1 = Cuboid.new(3,3,3,6,6,6)
+      cube2 = Cuboid.new(0,0,0,10,10,10)
+      expect(cube1.intersects?(cube2)).to be(true)
+      expect(cube2.intersects?(cube1)).to be(true)
+    end
+
+    it "returns false for two cuboids that are far apart" do
+      cube1 = Cuboid.new(3,3,3,6,6,6)
+      cube2 = Cuboid.new(100,100,100,5,5,5)
+      expect(cube1.intersects?(cube2)).to be(false)
+      expect(cube2.intersects?(cube1)).to be(false)
+    end
+
+    it "detects when one cuboid is inside another cuboid" do
+      cube1 = Cuboid.new(10,10,10,20,20,20)
+      cube2 = Cuboid.new(5,5,5,1,1,1)
+      expect(cube1.intersects?(cube2)).to be(true)
+      expect(cube2.intersects?(cube1)).to be(true)
+    end
+
+    it "allows for two cuboids to be adjacent (0 distance from another)" do
+      cube1 = Cuboid.new(10,10,10,20,20,20)
+      cube2 = Cuboid.new(10,10,30,20,20,20)
+      expect(cube1.intersects?(cube2)).to be(false)
+      expect(cube2.intersects?(cube1)).to be(false)
+    end
+  end
+
+  describe "#rotatex!" do
+    it "rotates cuboid 90 degrees about the x (height) axis" do
+      cube = Cuboid.new(10,10,10,5,10,4)
+      expect(cube.width).to eq(10)
+      expect(cube.len).to eq(4)
+      cube.rotatex!
+      expect(cube.center.values).to eq([10,10,10])
+      expect(cube.width).to eq(4)
+      expect(cube.len).to eq(10)
+    end
+  end
+
+  describe "#rotatex" do
+    it "returns true if rotation about x axis is possible" do
+      cube = Cuboid.new(10,10,10,5,10,4)
+      expect(cube.rotatex).to be (true)
+    end
+
+    it "rotates about x axis if there is room within default boundary" do
+      cube = Cuboid.new(10,10,10,5,10,4)
+      expect(cube.width).to eq(10)
+      expect(cube.len).to eq(4)
+      cube.rotatex
+      expect(cube.center.values).to eq([10,10,10])
+      expect(cube.width).to eq(4)
+      expect(cube.len).to eq(10)
+    end
+
+    it "rotates about x axis if there is room within arbritrary boundary" do
+      bounds = { x: 4, y: 5, z: 10 }
+      cube = Cuboid.new(10,10,14,6,6,8)
+      expect(cube.width).to eq(6)
+      expect(cube.len).to eq(8)
+      cube.rotatex
+      expect(cube.center.values).to eq([10,10,14])
+      expect(cube.width).to eq(8)
+      expect(cube.len).to eq(6)
+    end
+
+    it "does not rotate if rotation causes cuboid to overlap boundary" do
+      bounds = { x: 4, y: 5, z: 10 }
+      old_width = 6
+      old_length = 8
+      cube = Cuboid.new(4,5,10,6,old_width, old_length)
+      cube.rotatex(bounds)
+      expect(cube.width).to eq(old_width)
+      expect(cube.len).to eq(old_length)
+    end
+
+    it "returns center minimum distance from boundary required for rotation" do
+      cube = Cuboid.new(3,3,4,6,6,8)
+      new_center = cube.rotatex
+      old_width = 6
+      old_length = 8
+      expect(cube.rotatex).to eq([3,5,5])
+      expect(cube.len).to eq(old_length)
+      expect(cube.width).to eq(old_width)
+      expect(cube.move_to(3,5,5)).to be(true)
+      expect(cube.rotatex).to be(true)
+      expect(cube.width).to eq(old_length)
+      expect(cube.len).to eq(old_width)
+    end
+  end
 end
